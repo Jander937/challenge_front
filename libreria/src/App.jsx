c1402d60aee81f53f136ebd6740ce85a1db15c28
Jander937 <joburiticaga@cesde.net>
2024-04-03 19:33:46 -0500

    books

--- libreria/src/App.jsx
+++ libreria/src/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Prueba from './components/prueba/Prueba'
+import Cards from './components/Cards/Cards'
+import CreateBookButton from './components/CreateBookButton/CreateBookButton'
+import Carouse from "./components/Carousel/Carouse";
 
-
-
+const handleBookCreated = () => {
+  fetchData(); // Fetch the updated book list after a new book is created
+};
 
 
 function App() {
   return (
 <>
-
-<Prueba/>
-
+<Carouse/>
+  <CreateBookButton onBookCreated={handleBookCreated} />
+<Cards/>
 </>
   )
 }
--- libreria/src/components/Cards/Cards.jsx
+++ libreria/src/components/Cards/Cards.jsx
@@ -1,197 +1,88 @@
 import React, { useState, useEffect } from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import DeleteButton from '../DeleteButton/DeleteButton'; // Make sure this path is correct
+import EditBookRow from "../EditBookRow/EditBookRow"; // Make sure this path is correct
+import SearchBooks from '../SearchBooks/SearchBooks'; // Make sure this path is correct
+import './Cards.css'; // Adjust the path according to your file structure
 
-const Cards = () => {
+function Prueba() {
     const [bookData, setBookData] = useState([]);
     const [editingBook, setEditingBook] = useState(null);
-    const [newBook, setNewBook] = useState({
-        title: "",
-        isbn: "",
-        publication_year: ""
-    });
-    const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerm, setSearchTerm] = useState('');
 
     useEffect(() => {
         fetchData();
     }, []);
 
+    const handleUpdateBook = async (book) => {
+        // ... existing logic for updating a book
+    };
+
+    const cancelEdit = () => {
+        setEditingBook(null);
+    };
+
     const fetchData = async () => {
         try {
-            const response = await fetch('http://localhost:8080/api/v5/book/seach');
+            const response = await fetch(`http://localhost:8080/api/v5/book/seach`);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
             const data = await response.json();
             setBookData(data);
-            console.log(data)
         } catch (error) {
             console.error('Error al obtener los libros:', error);
         }
     };
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setNewBook(prev => ({
-            ...prev,
-            [name]: value
-        }));
+    const onDeletionSuccess = (id_book) => {
+        setBookData(bookData.filter(book => book.id_book !== id_book));
     };
 
-    const handleCreateBook = async (event) => {
-        event.preventDefault();
-
-        try {
-            const response = await fetch('http://localhost:8080/api/v5/book/save', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(newBook),
-            });
-
-            if (response.ok) {
-                console.log('Libro creado con éxito');
-                fetchData(); // Recarga los datos para reflejar los cambios
-                setNewBook({
-                    title: "",
-                    isbn: "",
-                    publication_year: ""
-                }); // Limpia los campos del formulario
-            } else {
-                console.error('Error al crear el libro:', await response.text());
-            }
-        } catch (error) {
-            console.error('Error al crear el libro:', error);
-        }
+    const handleSearch = (term) => {
+        setSearchTerm(term.toLowerCase());
     };
 
-    const handleEdit = (bookId) => {
-        const bookToEdit = bookData.find(book => book.id_book === bookId);
-        setEditingBook(bookToEdit);
-    };
-
-    const handleUpdateBook = async (event) => {
-        event.preventDefault();
-        console.log('Enviando datos al servidor:', editingBook);
-
-        try {
-            const response = await fetch(`http://localhost:8080/api/v5/book/${editingBook.id_book}`, {
-                method: 'PUT',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(editingBook),
-            });
-
-            if (response.ok) {
-                console.log('Libro actualizado con éxito');
-                fetchData(); // Recarga los datos para reflejar los cambios
-                setEditingBook(null); // Cierra el formulario de edición
-            } else {
-                console.error('Error al actualizar el libro:', await response.text());
-            }
-        } catch (error) {
-            console.error('Error al actualizar el libro:', error);
-        }
-    };
-
-    const handleDelete = async (id_book) => {
-        if (window.confirm('¿Estás seguro de que deseas eliminar este libro?')) {
-            try {
-                const response = await fetch(`http://localhost:8080/api/v5/book/delete/${id_book}`, {
-                    method: 'DELETE',
-                });
-
-                if (response.ok) {
-                    setBookData(bookData.filter(book => book.id_book !== id_book));
-                } else {
-                    console.error('Error al eliminar el libro:', await response.text());
-                }
-            } catch (error) {
-                console.error('Error al eliminar el libro:', error);
-            }
-        }
-    };
-
-    const handleSearch = async () => {
-        try {
-            const response = await fetch(`http://localhost:8080/api/v5/book/title?title=${searchTerm}`);
-            const data = await response.json();
-            setBookData(data);
-        } catch (error) {
-            console.error('Error al buscar libros:', error);
-        }
-    };
+    const filteredBooks = searchTerm
+        ? bookData.filter(book =>
+            book.title.toLowerCase().includes(searchTerm) ||
+            book.isbn.toLowerCase().includes(searchTerm) ||
+            book.publication_year.toString().toLowerCase().includes(searchTerm)
+          )
+        : bookData;
 
     return (
-        <div>
-            {/* Formulario para crear un nuevo libro */}
-            <form onSubmit={handleCreateBook}>
-                <input
-                    type="text"
-                    name="title"
-                    value={newBook.title}
-                    onChange={handleInputChange}
-                    placeholder="Título"
-                    required
-                />
-                <input
-                    type="text"
-                    name="isbn"
-                    value={newBook.isbn}
-                    onChange={handleInputChange}
-                    placeholder="ISBN"
-                    required
-                />
-                <input
-                    type="text"
-                    name="publication_year"
-                    value={newBook.publication_year}
-                    onChange={handleInputChange}
-                    placeholder="Año de publicación"
-                    required
-                />
-                <button type="submit">Crear libro</button>
-            </form>
-
-            {/* Barra de búsqueda */}
-            <input
-                type="text"
-                placeholder="Buscar por título"
-                value={searchTerm}
-                onChange={(e) => {
-                    setSearchTerm(e.target.value);
-                    handleSearch(); // Llama a la función de búsqueda cada vez que cambia el término de búsqueda
-                }}
-            />
-
-            {/* Tabla de libros existentes */}
-            <table>
-                <thead>
-                    <tr>
-                        <th>ISBN</th>
-                        <th>Título</th>
-                        <th>Año de publicación</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {bookData.map((book) => (
-                        <tr key={book.id_book}>
-                            <td>{book.isbn}</td>
-                            <td>{book.title}</td>
-                            <td>{book.publication_year}</td>
-                            <td>
-                    {/* Aquí estableces la URL de la imagen como el src del elemento img */}
-                    <img src={book.image} alt="Imagen del libro" />
-                </td>
-                          
-                            <td>
-                                <button onClick={() => handleEdit(book.id_book)}>Editar</button>
-                                <button onClick={() => handleDelete(book.id_book)}>Eliminar</button>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+        <div id="lucho">
+            <SearchBooks onSearch={handleSearch} />
+            {filteredBooks.map((book) => {
+                if (editingBook && book.id_book === editingBook.id_book) {
+                    return (
+                        <EditBookRow
+                            key={book.id_book}
+                            book={editingBook}
+                            onSave={handleUpdateBook}
+                            onCancel={cancelEdit}
+                        />
+                    );
+                } else {
+                    return (
+                        <Card key={book.id_book} style={{ width: '18rem' }}>
+                            <Card.Img variant="top" src={book.image} />
+                            <Card.Body>
+                                <Card.Title>{book.title}</Card.Title>
+                                <Card.Text>
+                                    Año de publicación: {book.publication_year}
+                                </Card.Text>
+                                <Button variant="primary" onClick={() => setEditingBook(book)}>Editar</Button>
+                                <DeleteButton id_book={book.id_book} onDeletionSuccess={onDeletionSuccess} />
+                            </Card.Body>
+                        </Card>
+                    );
+                }
+            })}
         </div>
     );
-};
+}
 
-export default Cards;
+export default Prueba;
